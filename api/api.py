eb3106b986600c553f3d2a5189ad786ea71db8c8
JakevK <jakevankeulen@gmail.com>
2020-05-29 10:35:03 +1200

    structure

---
+++ api/api.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, current_app, jsonify
+import os, json
+
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/presets', methods=['GET', 'POST'])
+def handle_presets():
+    if request.method == 'GET':
+        presets_file = os.path.join(current_app.root_path, 'storage', 'presets.json')
+
+        presets = json.load(open(presets_file))
+
+        return presets
+
+    elif request.method =='POST':
+        presets_file = os.path.join(current_app.root_path, 'storage', 'presets.json')
+
+        data = request.json
+        
+        with open(presets_file, 'w') as out:
+            json.dump(data, out)
+
+        return 'Success'
+
+    else:
+        return "Invalid method"
